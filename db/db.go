eefac5370bcb2593fe43234595053673224856ec
Ivan Terekh <ivan.terekh@gmail.com>
2018-12-06 23:12:25 +0300

    Fixed bug from Issue #15, added sqlx usage.

--- db/db.go
+++ db/db.go
@@ -1,30 +1,48 @@
 package db
 
 import (
-	"database/sql"
 	// Importing mysql driver.
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"log"
 	"os"
 )
 
-func init() {
-	db := openDb()
-	defer db.Close()
-	err := db.Ping()
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Printf("DB connection: OK. Host: %s:%s", os.Getenv("DB_IP"), os.Getenv("DB_PORT"))
-}
+var db *sqlx.DB
 
-func openDb() *sql.DB {
-	db, err := sql.Open("mysql",
+var stmts = struct {
+	insertResults   *sqlx.Stmt
+	selectSessionID *sqlx.Stmt
+}{}
+
+func init() {
+	var err error
+	db, err = sqlx.Open("mysql",
 		os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+
 			"@tcp("+os.Getenv("DB_IP")+":"+os.Getenv("DB_PORT")+")/ttimer")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return db
+	err = db.Ping()
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Printf("DB connection: OK. Host: %s:%s", os.Getenv("DB_IP"), os.Getenv("DB_PORT"))
+
+	err = prepareStatements()
+}
+
+func prepareStatements() error {
+	insertResults, err := db.Preparex(
+		"INSERT INTO results(sessionID, centis, scramble, penalty, datetime) VALUES(?, ?, ?, ?, ?)")
+	stmts.insertResults = insertResults
+	if err != nil {
+		return err
+	}
+
+	selectSessionID, err := db.Preparex(
+		"SELECT sessions.id FROM sessions WHERE sessions.userID = ? AND sessions.name = ? AND sessions.event = ?")
+	stmts.selectSessionID = selectSessionID
+	return err
 }
